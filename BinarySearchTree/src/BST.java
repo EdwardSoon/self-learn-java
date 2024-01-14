2d74778d3e0a20d2db143004dd7c3930ec61304f
edward <edward.soonrw@shopee.com>
2024-01-14 23:32:46 +0800

    bst add-on find()

--- BinarySearchTree/src/BST.java
+++ BinarySearchTree/src/BST.java
@@ -27,27 +27,29 @@ public class BST {
         }
     }
 
-    // for traversing binary from the Root node to the final node
-    // can be applied for all other operations
-    // will return the final Node
-    private Node binaryDivision(Node curr_node, int data){
+    /* binaryInsert method
+     - for traversing binary from the Root node to the final node
+     - for inserting mainly (also possibly used at other method)
+     - will return the final Node (for all the recursive function calls to exit)
+     */
+    private Node binaryInsert(Node curr_node, int data){
 
         if (data > curr_node.data){
             if (curr_node.right != null){
-                return binaryDivision(curr_node.right, data);
-                // the above means returning the result of binaryDivision(curr_node.right,data) recursively
+                return binaryInsert(curr_node.right, data); // return to have an exit for the current method call on stack memory
+                // the above means returning the result of binaryInsert(curr_node.right,data) recursively
                 // by recursive which eventually will reach the base case and return as `curr_node` for all the recursive function calls returns to exit all function calls
             }
             else{
-                return curr_node;
+                return curr_node;   // mostBottom node (final node) is returned
             }
         }
         else {  //  data <= curr_node.data
             if (curr_node.left != null){
-                return binaryDivision(curr_node.left, data);
+                return binaryInsert(curr_node.left, data);
             }
             else{
-                return curr_node;
+                return curr_node; // mostBottom node (final node) is returned
             }
         }
 //        return curr_node;
@@ -60,7 +62,7 @@ public class BST {
             return;
         }
         // always start from root to check, as we need to make all the values that are less than root.data to the left subtree, and bigger one to the right subtrees.
-        Node currFinalNode = binaryDivision(this.root, data); // division from root to curr final node
+        Node currFinalNode = binaryInsert(this.root, data); // division from root to curr final node
         // since the curr_node returned is the final node, we need to place the new node to either its right or left
         if (new_node.data > currFinalNode.data){
             currFinalNode.right = new_node;
@@ -70,24 +72,34 @@ public class BST {
         }
     }
 
-//    public void find(int data){
-//        // base case
-//        if (data == this.root.data){
-//            System.out.println(this.root.data + " exists.");
-//            return;
-//        }
-//
-//        Node currFinalNode = binaryDivision(this.root, data);
-//
-//        if (data > temp.data){
-//
-//        }
-//
-//        else if (data < temp.data){
-//
-//        }
-//
-//    }
+    private Node binaryFind(Node currNode, int data){
+        // base case
+        if (data == currNode.data){
+            System.out.println(currNode.data + " exists.");
+            return currNode;
+        }
+
+        if (data > currNode.data){
+            if (currNode.right != null){
+                return binaryFind(currNode.right, data);
+            }
+            System.out.println(data + " not found.");
+            return currNode;
+
+        }
+        else{ // data <= currNode.data
+            if (currNode.left != null){
+                return binaryFind(currNode.left, data);
+            }
+            System.out.println(data + " not found.");
+            return currNode;
+        }
+
+    }
+
+    public void find(int data){
+        binaryFind(this.root, data);
+    }
 
     public static void main(String[] args) {
         BST test = new BST();
@@ -95,20 +107,33 @@ public class BST {
         test.insert(2);
         test.insert(6);
         test.insert(4);
-//        test.insert(1);
-//        test.insert(3);
-//        test.insert(7);
-//        test.insert(8);
-//        test.insert(0);
+        test.insert(1);
+        test.insert(3);
+        test.insert(7);
+        test.insert(8);
+        test.insert(0);
+        test.insert(-1);
 
-        System.out.println("root:" + test.root.data);
-        System.out.println("r:" + test.root.right.data);
+//        System.out.println("root:" + test.root.data);
+//        System.out.println("r:" + test.root.right.data);
 //        System.out.println("rl:" + test.root.right.left.data);
 //        System.out.println("rll:" + test.root.right.left.left.data);
 //        System.out.println("l:" + test.root.left.data);
 //        System.out.println("rr:" + test.root.right.right.data);
 //        System.out.println("rrr:" + test.root.right.right.right.data);
 //        System.out.println("ll:" + test.root.left.left.data);
+        test.find(1);
+        test.find(0);
+        test.find(-1);
+        test.find(2);
+        test.find(3);
+        test.find(4);
+        test.find(5);
+        test.find(6);
+        test.find(7);
+        test.find(8);
+        test.find(9);
+        test.find(10);
 
     }
 }
