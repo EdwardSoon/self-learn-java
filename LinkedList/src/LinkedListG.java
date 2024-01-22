b33c6bca92044d293affbda13c20f722359fbb63
edward <edward.soonrw@shopee.com>
2024-01-22 22:59:38 +0800

    LinkedList added Generics

--- LinkedList/src/LinkedListG.java
+++ LinkedList/src/LinkedListG.java
@@ -1,18 +1,22 @@
 // https://www.geeksforgeeks.org/implementing-a-linked-list-in-java-using-class/
 
+import java.awt.*;
+import java.util.ArrayList;
+
 // it seems it is better to have non-static to implement as most methods are object specific based
-public class LinkedList {
+public class LinkedListG<E>{
 
-    Node head; // declare head Node of the list (as the object of Node and instance field/attributes of LinkedList) (using the class Node down there)
+    Node<E> head; // declare head Node of the list (as the object of Node and instance field/attributes of LinkedList) (using the class Node down there)
 
-    // sub-class (nested class) -- need sub-class because in an object of list it consists of Nodes objects.
+    // sub-class (nested class) --
+    // need sub-class because in an object of list it consists of Nodes objects.
     // created as a static class so that main can access it without object
-    static class Node{
-        int data;
-        Node next;  // object of Node for the next Node
+    static class Node<E>{
+        E data;
+        Node<E> next;  // object of Node for the next Node
 
         // constructor
-        Node(int d){
+        Node(E d){
             data = d;
             next = null; // default the next node is a null -- no need to declare also can, because it hasnt been instantiated so it is null
         }
@@ -26,28 +30,29 @@ public class LinkedList {
          * make a new_node there 
          * return list (since it is LinkedList object to return for the insert method)
          */
-    public static LinkedList insert(LinkedList list , int data){
+    public void insert(E data){  // we shouldn't provide
         // create a new node with given data
-        Node new_node = new Node(data);
-
+        Node<E> new_node = new Node<>(data);
+        System.out.println(data);
         // traverse to the last node and add the new_node there
-        if (list.head == null){     // if the head Node is null means it hasnt been instantiated anything, else it will have an address (not the data, to access the data it needs to be list.head.data)
-            list.head = new_node;   // thus we assigned the instantiated object to the head node, that way head node is in
+        if (this.head == null){     // if the head Node is null means it hasnt been instantiated anything, else it will have an address (not the data, to access the data it needs to be list.head.data)
+            this.head = new_node;   // thus we assigned the instantiated object to the head node, that way head node is in
         }
         else {
-            Node last = list.head; // declare a Last Node: this is necessary for traverse, if not we cant identify the last node
+            Node<E> last = this.head; // declare a Last Node: this is necessary for traverse, if not we cant identify the last node
             while(last.next != null){ // traverse process
                 last = last.next;   // if it is not null then last node will == next node
             }
             last.next = new_node;        // until it is last.next == null then we will have new_node assigned to last.next node (NOT last node, if not it will replace the last node but not inserting to the last.next node)
         }
-
-        return list;
+        
     }
 
-    // print list method 
-    public static void printList(LinkedList list){
-        Node currNode = list.head; // instantiate the currNode for the current Node
+    // print list method
+    // a static method thus we need to declare the working Generic Type (as it doesnt infer from Generic Class)
+    // (Note that: I purposely changed to T instead of using E, so that we show that this Method Generic declaration is on its own, not inferred by class)
+    public static <T> void printList(LinkedListG<T> list){
+        Node<T> currNode = list.head; // instantiate the currNode for the current Node
 
         System.out.print("LinkedList: ");
 
@@ -67,38 +72,30 @@ public class LinkedList {
         4. if it is the last node to be deleted, then made the prev node of its next node to be null 
         5. need the process of traverse to check the conditions
     */
-    public static LinkedList deleteByKey(LinkedList list, int key){ 
+    public void deleteByKey(E key){
         
-        Node currNode = list.head;
-        Node prev = null;
+        Node<E> currNode = this.head;
+        Node<E> prev = null;
 
         // Case 1: head node.data == key
-        if (currNode != null && currNode.data == key){
-            list.head = currNode.next;
+        if (currNode != null && currNode.data.equals(key)){     // use the equals as key is an object now instead of a primitive type
+            this.head = currNode.next;
             System.out.println(key + " found and deleted");
-            return list;
+            return;
         }
 
-        // Case 2: middle node.data == key
-        while (currNode != null && currNode.data != key){       // while currNode exists AND currNode data != key
+        // Case 2: middle node.data or Last node data== key
+        while (currNode != null && !currNode.data.equals(key)){       // while currNode exists AND currNode data != key
             prev = currNode;                // store into previous node so that later can replace it 
             currNode = currNode.next;       // currNode traverse to the next Node
         }
-        if (currNode.next != null && currNode.data == key){  // ensure it is not the last node && key found
+        if (currNode != null){  // ensure currNode is not null that means key is found (as null doesnt have data)
             prev.next = currNode.next; // to replace the linked of previous Node's next to the current Node with the next of the CurrentNode
             System.out.println(key + " found and deleted");
-            return list;
+            return;
         }
-        
-        // Case 3: last node.data == key
-        if (currNode.next == null && currNode.data == key){   // last node will have no next node && key found
-            prev.next = null;              // thus the previous node's next node replace with null and cut the link
-            System.out.println(key + " found and deleted");
-            return list;
-        }
-        
+        // the key is not found
         System.out.println("No key found");
-        return list;
     }
 
 
@@ -110,62 +107,77 @@ public class LinkedList {
             3. last
         * since there it is identified through index, thus we need to get index as parameter and we need to count our own index as it hits then will perform the same operations
         */
-    public static LinkedList deleteByNum(LinkedList list, int i){
-        Node currNode = list.head;
-        Node prev = null;
+    public void deleteByNum(int i){
+        Node<E> currNode = this.head;
+        Node<E> prev = null;
         int index = 0;
 
         // Case 1: head node.data == key
         if (currNode != null && index == i){
-            list.head = currNode.next;
+            this.head = currNode.next;
             System.out.println(i + " position element deleted");
-            return list;
+            return;
         }
         
-        // Case 2: middle node.data == key
+        // Case 2: middle node.data == key or last node.data == key
         while (currNode != null && index != i){       // while currNode exists AND currNode data != key
             prev = currNode;                // store into previous node so that later can replace it 
             currNode = currNode.next;       // currNode traverse to the next Node
             index ++;
         }
-        if (currNode != null && currNode.next != null && index == i){  // ensure it is not the last node && key found
+        if (currNode != null){  // ensure it is not null, then it must be the index == i
             prev.next = currNode.next; // to replace the linked of previous Node's next to the current Node with the next of the CurrentNode
             System.out.println(i + " position element deleted");
-            return list;
-        }
-        
-        // Case 3: last node.data == key
-        if (currNode != null && currNode.next == null && index == i){   // last node will have no next node && key found
-            prev.next = null;              // thus the previous node's next node replace with null and cut the link
-            System.out.println(i + " position element deleted");
-            return list;
+            return;
         }
         
         System.out.println("Index is beyond the bound");
-        return list;
-
     }
 
     public static void main(String[] args) throws Exception {
-        LinkedList ll = new LinkedList();
-        // System.out.println(ll);
+        LinkedListG<String> ll = new LinkedListG<>();
+        ll.insert("hi");
+        ll.insert("world");
+        ll.insert("me");
+        printList(ll);
+//        ll.deleteByKey("hi");
+        LinkedListG.<String>printList(ll);
+//        ll.deleteByKey("world");
+        printList(ll);
+//        ll.deleteByKey("me");
+        printList(ll);
+        ll.deleteByNum(2);
+        printList(ll);
+
 
         // System.out.println(LinkedList.insert(ll, 12));
-        insert(ll, 111);
-        insert(ll, 299);
-        LinkedList.insert(ll, 7123);
-        LinkedList.insert(ll, 1234);
+//        insert(ll, 111);
+//        insert(ll, 299);
+//        LinkedListG.insert(ll, 7123);
+//        LinkedListG.insert(ll, 1234)
+        
         // LinkedList.insert(ll, 23);
         // System.out.println(ll);
         // if (ll.head != null){
         //     System.out.println("non_empty");
         // }
-        printList(ll);
+//        printList(ll);
+//
+//        deleteByKey(ll, 111);
+//        printList(ll);
+//
+//        deleteByNum(ll, 0);
+//        printList(ll);
 
-        deleteByKey(ll, 111);
-        printList(ll);
+//        // what is the difference?
+//        ArrayList<Integer[]> al = new ArrayList<>();
+//        ArrayList<int[]> hi = new ArrayList<>();
+//
+//        al.add(new Integer[]{2,3,4});
+//        hi.add(new int[]{2,3,5});
+        
+        
 
-        deleteByNum(ll, 0);
-        printList(ll);
+
     }
 }
